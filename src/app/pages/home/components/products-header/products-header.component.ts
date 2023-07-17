96261e753172ad04d753102aa1db380af2bfc95b
Anton Rosh <rosh8442@gmail.com>
2023-07-17 16:39:54 -0700

    add home and products header components

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,18 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-products-header",
   templateUrl: "./products-header.component.html",
 })
 export class ProductsHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter();
   sort = "Descending";
   itemsShowCount = 12;
 
-  onSortUpdate(newSort: string): void {
+  onSortUpdated(newSort: string): void {
     this.sort = newSort;
   }
 
   onItemsUpdated(count: number): void {
     this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colNumber: number): void {
+    this.columnsCountChange.emit(colNumber);
   }
 }
